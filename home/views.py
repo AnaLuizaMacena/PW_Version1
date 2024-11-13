484ca675154e9d4c07d709395fb35a1255a0a1a6
Ana Luiza Macena Pereira <159857048+AnaLuizaMacena@users.noreply.github.com>
2024-11-13 17:15:21 -0300

    piloto

--- home/views.py
+++ home/views.py
@@ -7,7 +7,31 @@ def index (request):
     return render (request, 'index.html')
 
 def sobre (request):
-    return HttpResponse ('<h1>Sistema 1.0 desenvolvido por mesmo<h1>')
+    return render (request, 'sobre.html')
 
 def contato (request):
-    return HttpResponse ('Esta é a pagina de contato')
+    return render (request, 'contato.html')
+
+def exibir_item(request,id):
+    return render(request,"exibir_item.html",{'id':id})
+
+def perfil(request,usuario):
+    return render(request,"perfil.html",{'usuario':usuario})
+
+def diadasemana(request,id):
+    dias= {
+            1: 'Domingo',
+            2: 'Segunda-feira',
+            3: 'Terça-feira',
+            4: 'Quarta-feira',
+            5: 'Quinta-feira',
+            6: 'Sexta-feira',
+            7: 'Sabado'
+        }
+    dia= dias.get (id,None)
+
+    if dia:
+        return render (request, "diadasemana.html",{'dia': dia})
+    else:
+        return render (request, "diadasemana.html",{"dia": "dia invalido"})
+
